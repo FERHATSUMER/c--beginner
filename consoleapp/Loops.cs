aef4b8c71e9201063753701b4001855ede4e1c50
FERHATSUMER <sumerf39@gmail.com>
2024-01-02 22:26:13 +0300

    Added loops

---
+++ consoleapp/Loops.cs
@@ -0,0 +1,35 @@
+// int i = 0;
+// while (i < 10)
+// {
+//     Console.WriteLine(i);
+//     if (i == 5)
+//         break;
+//     i++;
+// }
+
+
+// for (int i = 1; i <= 10; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+
+// int i = 1;
+// while (i <= 10)
+// {
+//     Console.WriteLine(i);
+//     i++;
+// }
+
+// int i=1;
+// do
+// {
+//     Console.WriteLine(i);
+//     i++;
+// } while (i <= 10);
+
+string letter ="Ferhat";
+foreach(var i in letter){
+    Console.WriteLine(i);
+}
+
